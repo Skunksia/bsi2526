38dd8e27a0a1dea13f256cd29a70cbecb7d63da3
Skunksia <zboinafelina@gmail.com>
2025-11-23 19:10:20 +0100

    Create formularz.php

---
+++ zadanie_06/formularz.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+
+Imię: <?php echo $_POST["imię"]; ?><br>
+Nazwisko: <?php echo $_POST["nazwisko"]; ?><br>
+Data Urodzenia: <?php echo $_POST["datau"]; ?><br>
+Ulica: <?php echo $_POST["ulica"]; ?><br>
+Miasto: <?php echo $_POST["miasto"]; ?><br>
+Email: <?php echo $_POST["email"]; ?><br>
+Prawo Jazdy: <?php echo $_POST["prawo"]; ?><br>
+Płeć: <?php echo $_POST["płeć"]; ?><br>
+Uwagi: <?php echo $_POST["uwagi"]; ?><br>
+Nr telefonu: <?php echo $_POST["tel"]; ?><br>
+Województwo: <?php echo $_POST["woj"]; ?><br>
+</body>
+</html>
